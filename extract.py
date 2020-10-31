3d1b9f798e3d28eab82643779a059b47c55246fb
Atik Agarwal <40824902+Atik4@users.noreply.github.com>
2020-11-01 04:41:59 +0530

    Update extract.py

--- extract.py
+++ extract.py
@@ -2,7 +2,6 @@
 import requests
 
 def find_src_dest(query_string):
-
     query = query_string
     # query = "I want to travel from Bandra East Mumbai to Bandra West Mumbai"
     query_response_analyser = requests.post(
@@ -63,3 +62,37 @@ def find_src_dest(query_string):
 
     print(source, destination)
     return (source, destination)
+
+def find_src(query):
+
+    query_response_analyser = requests.post(
+        "http://api.intellexer.com/analyzeText?apiKey=25e240b9-cb35-47ec-9a80-25172c04e228",
+        data=query)
+
+    analyser_token = []
+    tokens = []
+    result = []
+
+    for i in query_response_analyser.json()['sentences'][0]['tokens']:
+        tokens.append(i['text']['content'])
+        analyser_token.append(i["partOfSpeechTag"])
+        result.append([i['text']['content'], i["partOfSpeechTag"]])
+    print("tokens", tokens)
+    print("analyser", analyser_token)
+    print("result", result)
+
+    i = 0
+    while i < len(result):
+        if result[i][1] == result[i - 1][1] and result[i][1] == 'NN':
+            result[i - 1][0] = result[i - 1][0] + " " + result[i][0]
+            result.remove(result[i])
+            tokens[i - 1] = tokens[i - 1] + tokens[i]
+            tokens.remove(tokens[i])
+            i = i - 1
+        i += 1
+    print(result)
+    for tuple in result:
+        if tuple[1] == 'NN':
+            print(tuple[0])
+            return tuple[0]
+    return ''
