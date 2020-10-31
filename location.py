a73566a709cc708904cb36ba7eea4c09e8cd5291
Atik Agarwal <40824902+Atik4@users.noreply.github.com>
2020-10-31 18:59:06 +0530

    Update location.py
    The navigation search query is merged with the maps API service inorder to find the desired routes to the destination.

--- location.py
+++ location.py
@@ -1,4 +1,80 @@
 import requests
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+
+query = "I am currently standing on Mata Mandir Bhopal get directions to DB Mall Bhopal"
+
+query_response_analyser = requests.post("http://api.intellexer.com/analyzeText?apiKey=25e240b9-cb35-47ec-9a80-25172c04e228",
+                               data=query)
+
+analyser_token =[]
+nouns=[]
+prepositions=[]
+tokens= []
+start_prepositions = ["from","on","at"]
+end_prepositions = ["to"]
+result=[]
+for i in query_response_analyser.json()['sentences'][0]['tokens']:
+    tokens.append(i['text']['content'])
+    analyser_token.append(i["partOfSpeechTag"])
+    result.append([i['text']['content'],i["partOfSpeechTag"]])
+    # if i["partOfSpeechTag"] in ["NN","NNS","NNP","NNPS"]:
+    #     nouns.append(i["text"]['content'])
+    if i["partOfSpeechTag"] in ["IN"]:
+        prepositions.append(i["text"]['content'])
+        if i["text"]["content"] in start_prepositions:
+            start = i["text"]["content"]
+        elif i["text"]["content"] in end_prepositions:
+            end = i["text"]["content"]
+
+
+
+print(query)
+print("tokens", tokens)
+print("analysers", analyser_token)
+print(result)
+
+start_index =   tokens.index(start)
+end_index =     tokens.index(end)
+
+for i in result[start_index+1:]:
+    if i[1] in ["NN","NNS","NNP","NNPS"]:
+        source=i[0]
+        break
+
+for i in result[end_index+1:]:
+    if i[1] in ["NN","NNS","NNP","NNPS"]:
+        destination=i[0]
+        break
+
+print(source, destination)
+# print(nouns)
+# print(prepositions)
+
+query_response_summeriser = requests.post("http://api.intellexer.com/summarizeText?apikey=3f772cc7-5773-4a2f-8f8d-a9da42a8ed3c&summaryRestriction=7&returnedTopicsCount=2&loadConceptsTree=true&loadNamedEntityTree=true&usePercentRestriction=true&conceptsRestriction=7&structure=general&fullTextTrees=true&textStreamLength=1000&wrapConcepts=true",
+                               data=query)
+summarize_token = []
+for i in query_response_summeriser.json()['items']:
+    summarize_token.append(i['text'])
+clusters=[]
+print(summarize_token)
+for i in summarize_token:
+    html_doc = i
+    page_soup = soup(html_doc, "html.parser")
+    containers = page_soup.findAll("b")
+
+    for container in containers:
+        clusters.append(container.text)
+
+    clusters.sort(reverse=True)
+    print(clusters)
+    for i in clusters:
+        if i.find(source)!=-1:
+            source = i
+        if i.find(destination)!=-1:
+            destination = i
+
+print(source, destination)
 
 
 location_from = "indore"
